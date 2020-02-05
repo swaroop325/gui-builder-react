b163d774ac2f8fea2d6ed4f87b52b25a9d46d1b7
swaroop <swaroopanand325@gmail.com>
2020-02-05 12:16:18 +0530

    Created base project with installed dependancies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
