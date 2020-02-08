99a85e6c814a769504e291f577168721f54318f2
swaroop <swaroopanand325@gmail.com>
2020-02-08 18:15:35 +0530

    Updates

---
+++ precache-manifest.0e7a106baa118cb3b5b73a69650b54ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3fe8ded8c8e65884547f17934ef345",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "444874f85b158ae7d7a7",
+    "url": "/my-app/static/css/2.7b050a0a.chunk.css"
+  },
+  {
+    "revision": "2b43dea59fde82c300d0",
+    "url": "/my-app/static/css/main.26fc6cbf.chunk.css"
+  },
+  {
+    "revision": "444874f85b158ae7d7a7",
+    "url": "/my-app/static/js/2.bd6f295c.chunk.js"
+  },
+  {
+    "revision": "b536546f38232599df0ec65278fefb4c",
+    "url": "/my-app/static/js/2.bd6f295c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b43dea59fde82c300d0",
+    "url": "/my-app/static/js/main.580a9481.chunk.js"
+  },
+  {
+    "revision": "aa5cfa1f14381f907f44",
+    "url": "/my-app/static/js/runtime-main.955b7274.js"
+  },
+  {
+    "revision": "45c73723862c6fc5eb3d6961db2d71fb",
+    "url": "/my-app/static/media/fontawesome-webfont.45c73723.eot"
+  },
+  {
+    "revision": "4b5a84aaf1c9485e060c503a0ff8cadb",
+    "url": "/my-app/static/media/fontawesome-webfont.4b5a84aa.woff2"
+  },
+  {
+    "revision": "76a4f23c6be74fd309e0d0fd2c27a5de",
+    "url": "/my-app/static/media/fontawesome-webfont.76a4f23c.svg"
+  },
+  {
+    "revision": "7c87870ab40d63cfb8870c1f183f9939",
+    "url": "/my-app/static/media/fontawesome-webfont.7c87870a.ttf"
+  },
+  {
+    "revision": "dfb02f8f6d0cedc009ee5887cc68f1f3",
+    "url": "/my-app/static/media/fontawesome-webfont.dfb02f8f.woff"
+  }
+]);
